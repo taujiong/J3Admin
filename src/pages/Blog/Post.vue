e263bf3ac0f278c2bf19a61a58029d2cef1ec3e0
taujiong <taujiong@outlook.com>
2021-05-16 23:24:43 +0800

    fix(Blogging): 添加 Post 页面的初步内容

--- src/pages/Blog/Post.vue
+++ src/pages/Blog/Post.vue
@@ -1,8 +1,16 @@
 <template>
-  <div v-for="post of posts" :key="post.id">{{ post.title }}</div>
+  <q-page class="column" padding>
+    <div class="text-h4">You have <span class="">{{ posts?.length }}</span> posts</div>
+    <div class="q-mt-xl">
+      <div v-for="post of posts" :key="post.id">
+        <PostAbstract :post="post" />
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script lang="ts">
+import PostAbstract from 'components/PostAbstract.vue';
 import { BlogPostDto } from 'src/models';
 import { PostService, PostServiceProvider } from 'src/services';
 import { provideIn } from 'src/utils';
@@ -10,6 +18,7 @@ import { defineComponent, onMounted, ref } from 'vue';
 
 export default defineComponent({
   name: 'Post',
+  components: { PostAbstract },
   setup() {
     let posts = ref<Array<BlogPostDto>>();
     const postService = provideIn<PostService>(PostServiceProvider);
